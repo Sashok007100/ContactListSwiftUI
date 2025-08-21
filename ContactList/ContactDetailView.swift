a43419f7ef649b72647ccc005e82370ec041f040
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-08-21 13:53:37 +0300

    Был создан экран с подробной информацией о контакте.

---
+++ ContactList/ContactDetailView.swift
@@ -0,0 +1,40 @@
+//
+//  ContactDetailView.swift
+//  ContactList
+//
+//  Created by Alexandr Artemov (Mac Mini) on 21.08.2025.
+//
+
+import SwiftUI
+
+struct ContactDetailView: View {
+    var mock: Person!
+    
+    var body: some View {
+        List {
+            Image(systemName: "person.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 100)
+                .frame(maxWidth: .infinity)
+                
+            HStack {
+                Image(systemName: "phone")
+                    .foregroundStyle(.tint)
+                Text(mock.phone)
+            }
+            
+            HStack {
+                Image(systemName: "tray")
+                    .foregroundStyle(.tint)
+                Text(mock.email)
+            }
+        }
+        .navigationTitle(mock.fullName)
+        .listStyle(.insetGrouped)
+    }
+}
+
+#Preview {
+    ContactDetailView(mock: Person.getMock()[0])
+}
