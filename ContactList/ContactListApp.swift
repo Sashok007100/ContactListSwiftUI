40ccc3a2c11cd19b0eefc892de32f423190fe292
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-08-21 11:47:48 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Alexandr Artemov (Mac Mini) on 21.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
